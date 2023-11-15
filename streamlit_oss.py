2b5b851781fe98f9e3bc34311c9dd8d10a6d8702
moon0109 <150204994+moon0109@users.noreply.github.com>
2023-11-15 11:12:12 +0900

    Add files via upload

--- streamlit_oss.py
+++ streamlit_oss.py
@@ -3,11 +3,10 @@ import streamlit as st
 #OSS 중산 시험 결과
 OSS_Score = [80, 70, 55, 30, 3, 3, 1, 0]
 
-st.write("# OSS 중간 시험 결과")
-st.write("## 얘들아 공부 좀 하자")
-st.write("### 집에도 일찍 가자")
+st.write("# 23114169")
+st.write("## 문준우")
 
 OSS_Score
 
 
-st.bar_chart(OSS_Score)
+st.area_chart(OSS_Score)
